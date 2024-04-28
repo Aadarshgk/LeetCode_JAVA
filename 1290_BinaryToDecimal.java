5db64bef06f7095de45fc6a192de6d673b9521d3
Aadarshgk <aadarshgk04@gmail.com>
2024-04-28 22:37:59 +0530

    Added Binary to decimal convertor

---
+++ 1290_BinaryToDecimal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode temp=head;
+        int sum=0;
+        while(temp!= null){
+            sum= (sum*2)+temp.val;
+            temp=temp.next;
+        }  
+        return sum;
+    }
+}
