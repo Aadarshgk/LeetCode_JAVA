bdc9faba54799fa3068db572b376cb2350a54c14
Aadarshgk <aadarshgk04@gmail.com>
2024-05-01 17:27:26 +0530

    Added Recursive and iterative approach to reversing linked list

--- 206_ReverseLL.java
+++ 206_ReverseLL.java
@@ -1,18 +1,29 @@
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head==null || head.next== null) return head;
+    public ListNode reverseListIterative(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
 
-        ListNode prev= null;
-        ListNode curr= head;
+        ListNode prev = null;
+        ListNode curr = head;
         ListNode next;
 
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }        
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
 
         return prev;
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode newHead = reverseListRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
