db4811127f531bd044a39441d05ddfdd88d48f0c
Aadarshgk <aadarshgk04@gmail.com>
2024-05-23 22:49:53 +0530

    Added 6 ZigZag Conversion program

--- 6_ZigZagConversion.java
+++ 6_ZigZagConversion.java
@@ -1,26 +1,47 @@
-import java.util.ArrayList;
-
 class Solution {
     public String convert(String s, int numRows) {
-        ArrayList<StringBuilder> ls=new ArrayList<>();
-        int i=0,len=s.length();
-        for(int j=0;j<numRows;j++){
-            ls.add(new StringBuilder());
-        }
-        while(i<len){
-            for(int j=0;j<numRows && i<len;j++,i++){
-                ls.get(j).append(s.charAt(i));
-            }
-
-            for(int j=numRows-2;j>0 && i<len;j--,i++){
-                ls.get(j).append(s.charAt(i));
-            }
-        }
+        if(numRows==1) return s;
+        int len=s.length(),count=2*(numRows-1),i,j,k;
         StringBuilder res= new StringBuilder();
-        for(StringBuilder a: ls){
-            res.append(a);
+        for(i=0;i<numRows;i++){
+            j=i;
+            k=count-2*i;
+            while(j<len){
+                res.append(s.charAt(j));
+                if(j+k<len && i!=0 && i!=numRows-1){
+                    res.append(s.charAt(j+k));
+                }
+                j+=count;
+            }
         }
         return res.toString();
-
     }
 }
+
+
+// import java.util.ArrayList;
+
+// class Solution {
+//     public String convert(String s, int numRows) {
+//         ArrayList<StringBuilder> ls=new ArrayList<>();
+//         int i=0,len=s.length();
+//         for(int j=0;j<numRows;j++){
+//             ls.add(new StringBuilder());
+//         }
+//         while(i<len){
+//             for(int j=0;j<numRows && i<len;j++,i++){
+//                 ls.get(j).append(s.charAt(i));
+//             }
+
+//             for(int j=numRows-2;j>0 && i<len;j--,i++){
+//                 ls.get(j).append(s.charAt(i));
+//             }
+//         }
+//         StringBuilder res= new StringBuilder();
+//         for(StringBuilder a: ls){
+//             res.append(a);
+//         }
+//         return res.toString();
+
+//     }
+// }
