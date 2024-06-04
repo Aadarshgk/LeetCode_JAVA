a8945cc448ac6d4e6d011dc325ec2820e5bd2b8d
Aadarshgk <aadarshgk04@gmail.com>
2024-06-04 20:23:54 +0530

    Added 61 Rotate list program

---
+++ 61_RotateList.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null) return null;
+        ListNode temp=head;
+        ListNode prev;
+        int count=1;
+        while(temp.next!=null){
+            temp=temp.next;
+            count++;
+        }
+        k=k%count;
+        while(k>0){
+            temp=head;
+            prev=null;
+            while(temp.next!=null){
+                prev=temp;
+                temp=temp.next;
+            }
+            temp.next=head;
+            head=temp;
+            prev.next=null;
+            k--;                      
+        }
+        return head;
+    }
+}
