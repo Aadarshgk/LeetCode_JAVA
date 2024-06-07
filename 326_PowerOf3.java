50e22c1b0031545b88845e6d0e35347d5d95ca68
Aadarshgk <aadarshgk04@gmail.com>
2024-06-07 19:02:35 +0530

    Added 326 Power of 3

---
+++ 326_PowerOf3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1) return false;
+        while(n>1){
+            if(n%3==0){
+                n/=3;
+            }else{
+                return false;
+            }
+        }
+        if(n==1) return true;
+        return false;
+    }
+}
