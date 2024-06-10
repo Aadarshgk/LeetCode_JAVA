b63e6ffcd8082e19527ed874281f0e377d524929
Aadarshgk <aadarshgk04@gmail.com>
2024-06-10 21:01:33 +0530

    Added 226 Invert Binary tree

---
+++ 226_InvertBinaryTree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+        TreeNode res=new TreeNode(root.val);
+        res.left=invertTree(root.right);
+        res.right=invertTree(root.left);
+        return res;
+    }
+}
