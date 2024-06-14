153804906fcba21ce37f765c17207d9d26fe42e9
Aadarshgk <aadarshgk04@gmail.com>
2024-06-14 21:39:17 +0530

    Added 365 Water and Jug probleam

---
+++ 365_WaterAndJug.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        if(x+y < target ) return false;
+        int t=Math.min(x,y);
+        while(x%t!=0 || y%t!=0){
+            t--;
+        }       
+
+        if(target%t==0) return true;
+        return false; 
+    }
+}
