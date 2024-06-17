9368e07cd397e625ad3a5e1712d3cf3bf1f66bb1
Aadarshgk <aadarshgk04@gmail.com>
2024-06-17 22:58:30 +0530

    Added 1 Two sum

--- 1_TwoSum.java
+++ 1_TwoSum.java
@@ -1,16 +1,16 @@
+// 
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i=0,j,n=nums.length;
-        while(i<n-1){
-            j=i+1;
-            while(j<n){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
-                j++;
+        int i=0,len=nums.length;
+        HashMap<Integer,Integer> set= new HashMap<>();
+        while(i<len){
+            if(set.containsKey(target-nums[i])){
+                return new int[]{i,set.get(target-nums[i])};
             }
+            set.put(nums[i],i);
             i++;
         }
-        return new int[]{};
+        return null;
     }
 }
