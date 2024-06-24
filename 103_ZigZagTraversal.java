a4549f50cc605726d32e7d7cdbe507b0735a6f97
Aadarshgk <aadarshgk04@gmail.com>
2024-06-24 22:52:33 +0530

    Added 103 Binary tree zigzag level order traversal

--- 103_ZigZagTraversal.java
+++ 103_ZigZagTraversal.java
@@ -1,29 +1,63 @@
+// class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         if(root==null) return new ArrayList<>();
+//         List<List<Integer>> res= new ArrayList<>();
+//         int count=0;
+//         ArrayList<TreeNode> q= new ArrayList<>();
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             List<Integer> ans= new ArrayList<>();
+//             int size= q.size();
+//             if(count%2==0){
+//                 for(int i=size-1;i>=0;i--){
+//                     TreeNode temp= q.remove(i);
+//                     if(temp.right!=null) q.add(temp.right);
+//                     if(temp.left!=null) q.add(temp.left);
+//                     ans.add(0,temp.val);
+//                 }
+//             }else{
+//                 for(int i=size-1;i>=0;i--){
+//                     TreeNode temp= q.remove(i);
+//                     if(temp.left!=null) q.add(temp.left);
+//                     if(temp.right!=null) q.add(temp.right);
+//                     ans.add(0,temp.val);
+//                 }
+//             }
+//             count++;
+//             res.add(ans);
+//         }
+//         return res;
+//     }
+// }
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if(root==null) return new ArrayList<>();
         List<List<Integer>> res= new ArrayList<>();
-        int count=0;
-        ArrayList<TreeNode> q= new ArrayList<>();
-        q.add(root);
+        if(root==null) return res;
+        boolean even=true;
+        Queue<TreeNode> q= new LinkedList<>();
+        TreeNode temp=root;
+        q.add(temp);
         while(!q.isEmpty()){
-            List<Integer> ans= new ArrayList<>();
+            List<Integer> ans= new LinkedList<>();
             int size= q.size();
-            if(count%2==0){
-                for(int i=size-1;i>=0;i--){
-                    TreeNode temp= q.remove(i);
-                    if(temp.right!=null) q.add(temp.right);
-                    if(temp.left!=null) q.add(temp.left);
-                    ans.add(0,temp.val);
-                }
-            }else{
-                for(int i=size-1;i>=0;i--){
-                    TreeNode temp= q.remove(i);
-                    if(temp.left!=null) q.add(temp.left);
-                    if(temp.right!=null) q.add(temp.right);
-                    ans.add(0,temp.val);
+            for(int i=0;i<size;i++){
+                temp= q.remove();
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
+
+                if(even) {
+                    ans.add(temp.val);
+                }else{
+                    ans.addFirst(temp.val);
                 }
             }
-            count++;
+            even= !even;
             res.add(ans);
         }
         return res;
