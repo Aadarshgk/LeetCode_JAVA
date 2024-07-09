d5c8afd90746e73e9f45effe6caa828a3d059ee3
Aadarshgk <aadarshgk04@gmail.com>
2024-07-09 22:34:42 +0530

    Added 27 Remove element

---
+++ 27_RemoveElement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len= nums.length,i=0;
+        int k=len;
+        while(i<len){
+            if(nums[i]==val){
+                replace(nums,i,len);
+                len--;
+            }else{
+                i++;
+            }
+        }
+        return len;
+    }
+    public void replace(int[] nums, int i, int len){
+        while(i<len-1){
+            nums[i]=nums[++i];
+        }
+    }
+}
