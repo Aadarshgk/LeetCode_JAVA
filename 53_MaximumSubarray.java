37c6bec900c4f4ec63e3237eacc3923fb13f3cb0
Aadarshgk <aadarshgk04@gmail.com>
2024-07-20 23:47:32 +0530

    Added 53 Maximum subarray

---
+++ 53_MaximumSubarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int temp= 0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int a:nums){
+            temp+=a;
+            if(maxSum<temp){
+                maxSum=temp;
+            }
+            if(temp<0){
+                temp=0;
+            }
+        }
+        return maxSum;
+    }
+}
