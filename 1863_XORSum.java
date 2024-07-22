57e4e06407363c3edca6403ed492d8a0ec5269c3
Aadarshgk <aadarshgk04@gmail.com>
2024-07-22 12:51:05 +0530

    Added 1863 Sum of all subset of XOR total

---
+++ 1863_XORSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int sum=0;
+        sum=subset(nums,0,sum);
+        return sum;
+    }
+
+    public int subset(int[] nums,int i, int total){
+        if(i==nums.length) return total;
+
+        return subset(nums,i+1, total^nums[i]) + subset(nums,i+1,total);
+    }
+}
