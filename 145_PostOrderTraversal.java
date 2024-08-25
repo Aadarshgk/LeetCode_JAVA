fec5cc7d767374f32f0bf5ca8743877b8896942c
Aadarshgk <aadarshgk04@gmail.com>
2024-08-25 09:04:22 +0530

    Added 145 Post Order

---
+++ 145_PostOrderTraversal.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    List<Integer> res= new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postOrder(root);
+        return res;
+    }
+
+    public void postOrder(TreeNode root){
+        if(root==null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        res.add(root.val);
+    } 
+}
