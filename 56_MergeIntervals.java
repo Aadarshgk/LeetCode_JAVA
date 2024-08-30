e3ffebc2d49b4af27942f4b213af81318f336537
Aadarshgk <aadarshgk04@gmail.com>
2024-08-30 20:50:03 +0530

    Added 56 Merge Intervals

---
+++ 56_MergeIntervals.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,Comparator.comparingInt(a-> a[0]));  
+        List<int[]> res= new ArrayList<>();
+        res.add(intervals[0]);
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]>res.get(res.size()-1)[1]){
+                res.add(intervals[i]);
+                continue;
+            }else{
+                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1], intervals[i][1]);
+            }
+        }
+
+        int[][] result= new int[res.size()][2];
+        for(int i=0;i<res.size();i++){
+            result[i]=res.get(i);
+        }
+        return result;
+    }
+}
