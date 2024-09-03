7d44965efe14f8fcdf97f8cb94a0d418247edaff
Aadarshgk <aadarshgk04@gmail.com>
2024-09-03 23:23:37 +0530

    added 169 Majority element

--- 169_MajorityElement.java
+++ 169_MajorityElement.java
@@ -1,15 +1,35 @@
-import java.util.Arrays;
+//most efficient soln- Boyer Moore's voting algorithm - O(n)
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int len=nums.length;
-        return nums[len/2];
+        int count=0,el1=0,len=nums.length;
+        for(int i=0;i<len;i++){
+            if(count==0){
+                el1=nums[i];
+            }
+            
+            if(el1==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return el1;
     }
 }
+
+//second solution - O(nlogn)
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         int len=nums.length;
+//         return nums[len/2];
+//     }
+// }
 //importance of understanding question and interpreting it rightly
 
 
 //initial idea
+// Arrays.sort(nums);
 // for(int i=0;i<len-1;i++){
 //     int count=0;
 //     while(i<len-1 && nums[i]==nums[i+1]){
