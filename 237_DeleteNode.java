125af039ac9b5094f1ebcbe6ed0163bb6aa9f985
Aadarshgk <aadarshgk04@gmail.com>
2024-09-14 21:03:59 +0530

    Added 237 Delete Node

--- 237_DeleteNode.java
+++ 237_DeleteNode.java
@@ -1,20 +1,29 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
+
+ //Best solution
 class Solution {
     public void deleteNode(ListNode node) {
-        ListNode temp=node;
-        ListNode prev=temp;
-        while(temp.next!=null){
-            temp.val=temp.next.val;
-            prev=temp;
-            temp=temp.next;
-        }
-        prev.next=null;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
+
+// class Solution {
+// public void deleteNode(ListNode node) {
+// ListNode temp=node;
+// ListNode prev=temp;
+// while(temp.next!=null){
+// temp.val=temp.next.val;
+// prev=temp;
+// temp=temp.next;
+// }
+// prev.next=null;
+// }
+// }
