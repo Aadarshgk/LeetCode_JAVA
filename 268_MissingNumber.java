4d20a8af665bc9744228cc1df6d272945b9675ad
Aadarshgk <aadarshgk04@gmail.com>
2024-09-29 18:57:58 +0530

    Added 268 Missing Number

--- 268_MissingNumber.java
+++ 268_MissingNumber.java
@@ -14,8 +14,8 @@ class Solution {
 // class Solution{
 //     public int missingNumber(int[] nums){
 //         int res=nums.length;
-//         for(int a: nums){
-//             res= (res^a);
+//         for(int i=0;i<nums.length;i++){
+//             res= (res^nums[i]^i);
 //         }
 //         return res;
 //     }
