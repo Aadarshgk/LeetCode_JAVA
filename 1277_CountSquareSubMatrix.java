bc4a85f3ea108ce104e103e5870342f1d7bf6352
Aadarshgk <aadarshgk04@gmail.com>
2024-10-29 22:26:47 +0530

    Added 1277 Count Square Sub matrix

---
+++ 1277_CountSquareSubMatrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int row=matrix.length,col=matrix[0].length;
+        int[][] dp=new int[row][col];
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==1){
+                    int left=0,top=0,dia=0;
+                    if(i==0 || j==0){
+                        dp[i][j]=1;
+                    }else{
+                        dp[i][j]=1 + Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                    }
+                    count+=dp[i][j];
+                }
+            }
+        }
+
+        return count;
+    }
+}
