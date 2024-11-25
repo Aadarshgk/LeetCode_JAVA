c4745991cdd1c025ea2c546ac7aac698d0717a03
Aadarshgk <aadarshgk04@gmail.com>
2024-11-25 18:33:47 +0530

    Added 16. 3Sum closest

---
+++ 16_3SumClosest.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int min=Integer.MAX_VALUE,len=nums.length;
+        for(int i=0;i<len;i++){
+            int j=i+1,k=len-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(Math.abs(sum-target)<Math.abs(min-target)){
+                    min=sum;
+                }
+                
+                if(sum<target){
+                    j++;
+                }else if(sum>target){
+                    k--;
+                }else{
+                    return sum;
+                }
+            }
+        }
+        return min;
+    }
+}
