c94d7ee8f520407410704ac648414af8fa584ec1
Aadarshgk <aadarshgk04@gmail.com>
2025-01-25 00:11:51 +0530

    Added 802 Find Eventual Safe States

--- 802_FindSafeStates.java
+++ 802_FindSafeStates.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int len=graph.length;
