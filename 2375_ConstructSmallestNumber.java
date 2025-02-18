9f30c65a2e12a5facd3dac26180d757062b33e83
Aadarshgk <aadarshgk04@gmail.com>
2025-02-18 21:24:55 +0530

    Added 2375. Construct Smallest Number From DI String

--- 2375_ConstructSmallestNumber.java
+++ 2375_ConstructSmallestNumber.java
@@ -48,3 +48,21 @@ class Solution {
         return false;
     }
 }
+
+//Stack soln.
+// class Solution {
+//     public String smallestNumber(String pattern) {
+//         Stack<Integer> stack=new Stack<>();
+//         int len=pattern.length();
+//         StringBuilder sb=new StringBuilder();
+//         for(int i=1;i<=len+1;i++){
+//             stack.push(i);
+//             if(i==len+1 || pattern.charAt(i-1)=='I'){
+//                 while(!stack.isEmpty()){
+//                     sb.append(stack.pop());
+//                 }
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
