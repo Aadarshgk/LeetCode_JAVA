407d4595f91089439ce1d6d55e57a3f076668aac
Aadarshgk <aadarshgk04@gmail.com>
2025-03-17 20:16:59 +0530

    Added 2206. Divide Array Into Equal Pairs

---
+++ 2206_DivideArrayIntoEqualPairs.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num: nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
+            }
+        }
+
+        return set.size()==0;
+    }
+}
